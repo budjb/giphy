418940c1331084f8d096219721f10f60c57e0c41
bud4453 <bud.byrd@rackspace.com>
2020-06-12 16:25:08 -0500

    Add sops for secrets.

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,19 @@ provider "aws" {
 data "aws_route53_zone" "budjb_com_zone" {
   name = "budjb.com"
 }
+
+data "aws_kms_key" "hegiphy" {
+  key_id = "arn:aws:kms:us-east-1:211643380035:key/32f6098f-5157-4625-ba23-75fe0117d986"
+}
+
+variable "giphy_api_key" {
+  type = string
+}
+
+resource "aws_ssm_parameter" "giphy_api_key" {
+  name      = "/hegiphy/giphy_api_key"
+  type      = "SecureString"
+  value     = var.giphy_api_key
+  key_id    = data.aws_kms_key.hegiphy.key_id
+  overwrite = true
+}
